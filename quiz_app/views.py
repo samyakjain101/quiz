1b1026cde6baaf7c8b1357b50d04d8049404c6dd
samyakjain101 <samyakjain101@gmail.com>
2020-10-12 16:30:25 +0530

    save_answer view updated

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -18,50 +18,6 @@ class AvailableQuiz(TemplateView):
         }
         return context
 
-# class LiveQuiz(TemplateView):
-#     template_name = "quiz_app/quiz_questions.html"
-#     def get_context_data(self, **kwargs):
-
-#         try:
-#             quiz_id = uuid.UUID(kwargs['quiz_id']).hex
-#         except ValueError:
-#             raise PermissionDenied()
-
-#         try: 
-#             quiz = Quiz.objects.get(id = quiz_id)
-#         except Quiz.DoesNotExist:
-#             raise PermissionDenied()
-
-#         # Using Paginator
-
-#         questions = quiz.question_set.all().order_by('question_number')
-#         paginator = Paginator(questions, 1) # Show 1 question per page.
-
-#         page_number = self.request.GET.get('page')
-#         if not page_number:
-#             page_number = 1
-#         page_obj = paginator.get_page(page_number)
-#         context = {
-#             'page_obj' : page_obj,
-#             'allPages' : paginator.page_range,
-#         }
-
-#         try:
-#             record = QuizRecord.objects.get(user=self.request.user, quiz = page_obj.object_list[0].quiz)
-#             answer = QuizAnswerRecord.objects.get(record = record, question = page_obj.object_list[0]).myAns
-#             context['answer'] = answer
-#         except (QuizRecord.DoesNotExist, QuizAnswerRecord.DoesNotExist):
-#             #If this exception is called this means user has not answered this question till now.
-#             #Do nothing
-#             pass
-
-#         all_status = [0]*questions.count()
-#         for x in QuizAnswerRecord.objects.filter(record = record).order_by("question__question_number"):
-#             all_status[x.question.question_number - 1] = x.status
-#         context['all_status'] = all_status
-        
-#         return context
-
 def attempt_quiz(request, **kwargs):
     # Check if quiz_id is valid uuid 
     try:
@@ -153,42 +109,23 @@ def save_answer(request):
         choice_id = request.GET.get('choice_id')
         
         try:
-            question = Question.objects.get(id=question_id)
+            question = QuizAnswerRecord.objects.get(id=question_id)
             if choice_id:   
                 choice = Choice.objects.get(id=choice_id)
             else:
                 choice = None
-            quiz_record = QuizRecord.objects.get(user=current_user, quiz=question.quiz)
-
-        except (Question.DoesNotExist, Choice.DoesNotExist, QuizRecord.DoesNotExist):
-            raise PermissionDenied()
-
-        try:
-            quiz = QuizAnswerRecord.objects.get(record=quiz_record, question=question)
 
             if to_do == "saveAndNext":
                 jsonr['message'] = "Choice Updated"
-                quiz.status = QuizAnswerRecord.SAVE_AND_NEXT
+                question.status = QuizAnswerRecord.SAVE_AND_NEXT
             elif to_do == "markForReview":
-                quiz.status = QuizAnswerRecord.MARK_FOR_REVIEW
+                question.status = QuizAnswerRecord.MARK_FOR_REVIEW
                 jsonr['message'] = "Marked For Review"
 
-            quiz.myAns = choice
-            quiz.save()
+            question.myAns = choice
+            question.save()
 
-        except QuizAnswerRecord.DoesNotExist:
-            if to_do == "saveAndNext":
-                jsonr['message'] = "Your response is saved"
-                status = QuizAnswerRecord.SAVE_AND_NEXT
-            elif to_do == "markForReview":
-                jsonr['message'] = "Marked For Review"
-                status = QuizAnswerRecord.MARK_FOR_REVIEW
-
-            QuizAnswerRecord.objects.create(
-                record=quiz_record, 
-                question=question,
-                myAns=choice,
-                status=status
-            )
+        except (Choice.DoesNotExist, QuizAnswerRecord.DoesNotExist):
+            raise PermissionDenied()
         
     return HttpResponse(json.dumps(jsonr), content_type='application/json')
